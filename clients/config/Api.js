81674a0aaca0e548bee252ed7e4347be309a6ad9
ajibadeabd <ajibadeabd@gmail.com>
2020-04-29 09:49:42 +0100

    new change the order page

--- clients/config/Api.js
+++ clients/config/Api.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 export default () => {
     return axios.create({
-        baseURL:
-         'http://localhost:3000'
-        // baseURL: '',
+        // baseURL:
+        //  'http://localhost:3000'
+        baseURL: '',
     });
 }
