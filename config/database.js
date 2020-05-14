ab8c4824aa60842739fd4f461361f8e5e86156f1
ajibadeabd <ajibadeabd@gmail.com>
2020-05-14 17:04:38 +0100

    added a paypal payment method

---
+++ config/database.js
@@ -0,0 +1,10 @@
+if(process.env.NODE_ENV === 'production'){
+    module.exports =  {mongoURL:
+        URI ="mongodb+srv://user:user@cluster0-ha9ym.mongodb.net/test?retryWrites=true&w=majority"
+ }
+}
+else{
+    module.exports =  {mongoURL:
+    'mongodb://localhost/Ali_express_clone'
+}
+}
